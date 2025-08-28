4981255693f70d4eda8b0448fd4ca2373c6d8e85
Cxnker <72629137+cxnker@users.noreply.github.com>
2025-08-28 14:53:36 -0400

    Update Fly.lua

--- Scripts/Fly.lua
+++ Scripts/Fly.lua
@@ -19,8 +19,8 @@ main.ResetOnSpawn = false
 frame.Parent = main
 frame.BackgroundColor3 = Color3.fromRGB(163, 255, 137)
 frame.BorderColor3 = Color3.fromRGB(103, 221, 213)
-frame.Position = UDim2.new(0.100320168, 0, 0.379746825, 0)
-frame.Size = UDim2.new(0, 189, 0, 56)
+frame.Position = UDim2.new(0.1, 0, 0.1, 0)
+frame.Size = UDim2.new(0, 189, 0, 54)
 
 closebtn.Name = "Close"
 closebtn.Parent = main.Frame
