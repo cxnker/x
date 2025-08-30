0a627a016733a8ca41ddb31f97aef8cca3671a53
Cxnker <72629137+cxnker@users.noreply.github.com>
2025-08-30 15:37:42 -0400

    Update FPS-Counter.lua

--- Scripts/FPS-Counter.lua
+++ Scripts/FPS-Counter.lua
@@ -1,26 +1,22 @@
 repeat wait() until game:IsLoaded() wait(2)
-local ScreenGui = Instance.new("ScreenGui")
+local Gui = Instance.new("ScreenGui")
 local Fps = Instance.new("TextLabel")
 
---Properties:
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Gui.Parent = game.CoreGui
+Gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Fps.Name = "Fps"
-Fps.Parent = ScreenGui
-Fps.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Fps.BackgroundTransparency = 1.000
-Fps.Position = UDim2.new(0.786138654, 0, 0, 0)
-Fps.Size = UDim2.new(0, 125, 0, 25)
+Fps.Parent = Gui
+Fps.BackgroundTransparency = 1
+Fps.Position = UDim2.new(0.9, 0, 0, 0)
+Fps.Size = UDim2.new(0,60,0,20)
 Fps.Font = Enum.Font.SourceSans
 Fps.TextColor3 = Color3.fromRGB(1,1,1)
 Fps.TextScaled = true
-Fps.TextSize = 14.000
+Fps.TextSize = 18
 Fps.TextWrapped = true
 
 -- Scripts:
 local script = Instance.new('LocalScript', Fps)
-local RunService = game:GetService("RunService")
-RunService.RenderStepped:Connect(function(frame) -- This will fire every time a frame is rendered
+game:GetService("RunService").RenderStepped:Connect(function(frame) -- This will fire every time a frame is rendered
 script.Parent.Text = ("FPS: "..math.round(1/frame)) 
 end)
