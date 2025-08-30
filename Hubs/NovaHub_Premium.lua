693fa868cfc812e154f3f056c2a113ecba4de56f
Cxnker <72629137+cxnker@users.noreply.github.com>
2025-08-30 13:54:55 -0400

    Update NovaHub_Premium.lua

--- Hubs/NovaHub_Premium.lua
+++ Hubs/NovaHub_Premium.lua
@@ -7,7 +7,7 @@ local Window = redzlib:MakeWindow({
   })
 
   Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://121315989944092", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://111486071468142", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
