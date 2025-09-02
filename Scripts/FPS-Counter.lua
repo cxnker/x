d5b453b010c260f25031e176dddc115979adb410
Cxnker <72629137+cxnker@users.noreply.github.com>
2025-09-01 22:10:50 -0400

    Update FPS-Counter.lua

--- Scripts/FPS-Counter.lua
+++ Scripts/FPS-Counter.lua
@@ -4,7 +4,7 @@ f.BackgroundTransparency = 1
 f.Position = UDim2.new(0.1,0,0,0)
 f.Size = UDim2.new(0,60,0,20)
 f.Font = Enum.Font.SourceSans
-f.TextColor3 = Color3.fromRGB(1,1,1)
+f.TextColor3 = Color3.fromRGB(255,255,255)
 f.TextSize = 18
 
 local lastUpdate = tick()
@@ -13,9 +13,10 @@ game:GetService("RunService").RenderStepped:Connect(function()
     frameCount = frameCount + 1
     local currentTime = tick()
     if currentTime - lastUpdate >= 1 then
-        f.Text = string.format("FPS: %.1f", frameCount / (currentTime - lastUpdate))
+        f.Text = string.format("FPS: %.0f", frameCount / (currentTime - lastUpdate))
         frameCount = 0
         lastUpdate = currentTime
     end
 end)
+
 
