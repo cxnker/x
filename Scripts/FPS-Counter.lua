7f0a9f1966bf43d9d16fb73d021882f4a584319c
Cxnker <72629137+cxnker@users.noreply.github.com>
2025-09-01 22:06:56 -0400

    Update FPS-Counter.lua

--- Scripts/FPS-Counter.lua
+++ Scripts/FPS-Counter.lua
@@ -1,7 +1,7 @@
 local f = Instance.new("TextLabel")
-f.Parent = Instance.new("ScreenGui", game.Players.LocalPlayer:WaitForChild("PlayerGui"))
+f.Parent = Instance.new("ScreenGui", game.CoreGui)
 f.BackgroundTransparency = 1
-f.Position = UDim2.new(0.9,0,0,0)
+f.Position = UDim2.new(0.1,0,0,0)
 f.Size = UDim2.new(0,60,0,20)
 f.Font = Enum.Font.SourceSans
 f.TextColor3 = Color3.fromRGB(1,1,1)
@@ -13,8 +13,9 @@ game:GetService("RunService").RenderStepped:Connect(function()
     frameCount = frameCount + 1
     local currentTime = tick()
     if currentTime - lastUpdate >= 1 then
-        f.Text = string.format("FPS: %f", frameCount / (currentTime - lastUpdate))
+        f.Text = string.format("FPS: %.1f", frameCount / (currentTime - lastUpdate))
         frameCount = 0
         lastUpdate = currentTime
     end
 end)
+
